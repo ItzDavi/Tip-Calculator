f2d98f8881f7500af84925709479284b6ca95138
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-04-26 21:49:56 +0200

    Added comments to the JS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 function calculate() {
+  //Initialize all the variables needed
+  //The bill amount given by the people
   var billAmount = document.getElementById('bill-amount').value;
-  var serviceQuality = document.getElementById('service-quality').value;
-  var tipSharing = document.getElementById('tip-sharing').value;
-  var tipSplitted = document.getElementById('tip-splitted');
-  var tipEach = document.getElementById('tip-each');
 
+  //The service quality rate using the option tags to get directly the value
+  var serviceQuality = document.getElementById('service-quality').value;
+
+  //Number of people dividing the total tip
+  var tipSharing = document.getElementById('tip-sharing').value;
+
+  //Amount of dollars of tip divided by the number of the people
+  var tipSplitted = document.getElementById('tip-splitted');
+
+  //Currently useless variable
+  //var tipEach = document.getElementById('tip-each');
+
+  //Check of the bill amount has value, if not AN AL-ERT will show up
   if (billAmount == 0 || billAmount === '') {
     alert('Please enter a value for the bill in numbers');
     return;
@@ -18,16 +29,23 @@ function calculate() {
   }
 }*/
 
+  //                 $$ of bill * 0 or 0.05 or 0.1... / number of people
   var tipSplitted = (billAmount * serviceQuality / tipSharing);
 
+  //Roundindingding to two decimals
   tipSplitted = Math.round(tipSplitted * 100) / 100;
+
+  //Fixing the two decimals position
   tipSplitted = tipSplitted.toFixed(2);
 
+  //Display the tip divided
   document.getElementById('tip-splitted').innerHTML = tipSplitted;
 }
 
+//Variable for the button
 var buttonCalculate = document.getElementById('button-calculate');
 
+//Onclick proprety to start the calculate() function when the button Calculate is pressed
 buttonCalculate.onclick = function() {
   calculate();
 }
